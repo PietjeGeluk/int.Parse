fd50d5f1903b82ce9a45d96eefb038bc16505d7e
PietjeGeluk <62058850+PietjeGeluk@users.noreply.github.com>
2020-03-18 20:05:09 +0100

    Add files via upload

---
+++ Cal.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Cal.aspx.cs" Inherits="CalculatorASP.Cal" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Getal 1:
+            <asp:TextBox ID="TextBox1" runat="server" Width="94px"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="btnKeer" runat="server" OnClick="btnKeer_Click" Text="X" />
+&nbsp;<asp:Button ID="btnDelen" runat="server" OnClick="btnDelen_Click" Text=":" />
+&nbsp;<asp:Button ID="btnEraf" runat="server" OnClick="btnEraf_Click" Text="-" />
+&nbsp;<asp:Button ID="btnPlus" runat="server" OnClick="btnPlus_Click" Text="+" />
+&nbsp;&nbsp;&nbsp;
+            <asp:Label ID="lblOperator" runat="server"></asp:Label>
+            <br />
+            <br />
+            Getal 2: <asp:TextBox ID="TextBox2" runat="server" Width="95px"></asp:TextBox>
+            <br />
+        </div>
+    &nbsp;<asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="   =   " />
+        <br />
+        <br />
+        Uitkomst:
+        <asp:TextBox ID="txtUitkomst" runat="server" Width="86px"></asp:TextBox>
+        <br />
+        <br />
+    </form>
+</body>
+</html>
---
+++ Cal.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CalculatorASP
+{
+    public partial class Cal : System.Web.UI.Page
+    {
+        double a, b, c;
+
+        protected void btnKeer_Click(object sender, EventArgs e)
+        {
+            lblOperator.Text = "x";
+        }
+
+        protected void btnDelen_Click(object sender, EventArgs e)
+        {
+            lblOperator.Text = "/";
+        }
+
+        protected void btnEraf_Click(object sender, EventArgs e)
+        {
+            lblOperator.Text = "-";
+        }
+
+        protected void btnPlus_Click(object sender, EventArgs e)
+        {
+            lblOperator.Text = "+";
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            a = double.Parse(TextBox1.Text);
+            b = double.Parse(TextBox2.Text);
+
+            if (lblOperator.Text == "x")
+            {
+                c = a * b;
+            }
+            else if (lblOperator.Text == "+")
+            {
+                c = a + b;
+            }
+            else if (lblOperator.Text == "-")
+            {
+                c = a - b;
+            }
+            else if (lblOperator.Text == "/")
+            {
+                c = a / b;
+            }
+            txtUitkomst.Text = c.ToString();
+        }
+    }
+}
